673c71bb1673f4f6ad781c346f07d26ffd01aa6c
Jason Reilly <jdr0887@gmail.com>
2015-08-05 10:52:48 -0400

    misc

--- ncbi-commands/src/main/java/org/renci/hearsay/commands/ncbi/PersistAlignmentsFromNCBIRunnable.java
+++ ncbi-commands/src/main/java/org/renci/hearsay/commands/ncbi/PersistAlignmentsFromNCBIRunnable.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.renci.gbff.model.Feature;
 import org.renci.gbff.model.Sequence;
@@ -56,10 +57,17 @@ public class PersistAlignmentsFromNCBIRunnable implements Runnable {
 
     private final Sequence sequence;
 
-    public PersistAlignmentsFromNCBIRunnable(final HearsayDAOBean hearsayDAOBean, final Sequence sequence) {
+    private final List<ReferenceSequence> referenceSequences;
+
+    private final Feature firstCDSFeature;
+
+    public PersistAlignmentsFromNCBIRunnable(final HearsayDAOBean hearsayDAOBean, final Sequence sequence,
+            final List<ReferenceSequence> referenceSequences, final Feature firstCDSFeature) {
         super();
         this.hearsayDAOBean = hearsayDAOBean;
         this.sequence = sequence;
+        this.referenceSequences = referenceSequences;
+        this.firstCDSFeature = firstCDSFeature;
     }
 
     @Override
@@ -70,53 +78,7 @@ public class PersistAlignmentsFromNCBIRunnable implements Runnable {
             String refSeqVersionedAccession = sequence.getVersion().trim().contains(" ") ? sequence.getVersion()
                     .substring(0, sequence.getVersion().indexOf(" ")) : sequence.getVersion();
 
-            List<Identifier> rnaNucleotideAccessionIdentifierList = hearsayDAOBean.getIdentifierDAO().findByExample(
-                    new Identifier("www.ncbi.nlm.nih.gov/nuccore", refSeqVersionedAccession));
-
-            String proteinAccession = null;
-            Feature firstCDSFeature = null;
-            for (Feature feature : sequence.getFeatures()) {
-                if (!"CDS".equals(feature.getType())) {
-                    continue;
-                }
-                firstCDSFeature = feature;
-                break;
-            }
-
-            proteinAccession = firstCDSFeature.getQualifiers().getProperty("protein_id").replace("\"", "");
-
-            List<Identifier> proteinAccessionIdentifierList = hearsayDAOBean.getIdentifierDAO().findByExample(
-                    new Identifier("www.ncbi.nlm.nih.gov/protein", proteinAccession));
-
-            List<Long> identifierIdList = new ArrayList<Long>();
-            for (Identifier identifier : rnaNucleotideAccessionIdentifierList) {
-                identifierIdList.add(identifier.getId());
-            }
-            for (Identifier identifier : proteinAccessionIdentifierList) {
-                identifierIdList.add(identifier.getId());
-            }
-
-            List<ReferenceSequence> potentialRefSeqs = hearsayDAOBean.getReferenceSequenceDAO().findByIdentifiers(
-                    identifierIdList);
-
-            if (potentialRefSeqs == null || (potentialRefSeqs != null && potentialRefSeqs.isEmpty())) {
-                logger.warn("Could not find ReferenceSequence: refSeqVersionedAccession = {}, proteinAccession = {}",
-                        refSeqVersionedAccession, proteinAccession);
-                return;
-            }
-
-            logger.info("Using ReferenceSequence: refSeqVersionedAccession = {}, proteinAccession = {}",
-                    refSeqVersionedAccession, proteinAccession);
-
-            final StrandType strandType = potentialRefSeqs.get(0).getStrandType();
-
-            String refSeqGenomicAccession = null;
-            for (Identifier identifier : potentialRefSeqs.get(0).getIdentifiers()) {
-                if (identifier.getSystem().equals("www.ncbi.nlm.nih.gov/genome")) {
-                    refSeqGenomicAccession = identifier.getValue();
-                    break;
-                }
-            }
+            final StrandType strandType = referenceSequences.get(0).getStrandType();
 
             // add protein info to alignment
             String firstCDSFeatureLocation = firstCDSFeature.getLocation();
@@ -146,24 +108,24 @@ public class PersistAlignmentsFromNCBIRunnable implements Runnable {
                 }
             }
 
-            if (sequence.getFeatures() == null || (sequence.getFeatures() != null && sequence.getFeatures().isEmpty())) {
-                int exonCount = 0;
+            int exonCount = 0;
+            if (CollectionUtils.isNotEmpty(sequence.getFeatures())) {
                 for (Feature feature : sequence.getFeatures()) {
                     if (!"exon".equals(feature.getType())) {
                         continue;
                     }
                     exonCount++;
                 }
-                if (exonCount == 0) {
-                    logger.warn("empty features");
-                    return;
-                }
+            }
+            if (exonCount == 0) {
+                logger.warn("empty features");
+                return;
             }
 
             // add alignments
             Alignment alignment = new Alignment();
             alignment.setId(hearsayDAOBean.getAlignmentDAO().save(alignment));
-            alignment.setReferenceSequences(potentialRefSeqs);
+            alignment.setReferenceSequences(referenceSequences);
 
             Location proteinLocation = new Location(firstCDSFeatureLocationStart, firstCDSFeatureLocationStop);
             proteinLocation.setId(hearsayDAOBean.getLocationDAO().save(proteinLocation));
@@ -205,66 +167,82 @@ public class PersistAlignmentsFromNCBIRunnable implements Runnable {
             hearsayDAOBean.getAlignmentDAO().save(alignment);
 
             List<Region> regions = alignment.getRegions();
-            Collections.sort(regions, new Comparator<Region>() {
-                @Override
-                public int compare(Region r1, Region r2) {
-                    if (strandType.equals(StrandType.MINUS)) {
-                        return r2.getTranscriptLocation().getStart().compareTo(r1.getTranscriptLocation().getStart());
-                    } else {
-                        return r1.getTranscriptLocation().getStart().compareTo(r2.getTranscriptLocation().getStart());
+            if (CollectionUtils.isNotEmpty(regions)) {
+                Collections.sort(regions, new Comparator<Region>() {
+                    @Override
+                    public int compare(Region r1, Region r2) {
+                        if (strandType.equals(StrandType.MINUS)) {
+                            return r2.getTranscriptLocation().getStart()
+                                    .compareTo(r1.getTranscriptLocation().getStart());
+                        } else {
+                            return r1.getTranscriptLocation().getStart()
+                                    .compareTo(r2.getTranscriptLocation().getStart());
+                        }
                     }
-                }
-            });
+                });
 
-            logger.info("refSeqGenomicAccession = {}", refSeqGenomicAccession);
-            for (String line : alignmentsLines) {
-
-                if (line.startsWith("#")) {
-                    continue;
-                }
-
-                // NT_187633.1 RefSeq cDNA_match 278295 278486 192 - . ID=aln45579;Target=NM_000853.3 1 192
-                // +;gap_count=0;identity=1;idty=1;num_ident=1093;num_mismatch=0;pct_coverage=100;pct_identity_gap=100;pct_identity_ungap=100;score=192
-                String[] columns = line.split("\t");
-                String genomicAccession = columns[0];
-                String genomicStart = columns[3];
-                String genomicStop = columns[4];
-                String strand = columns[6];
-                String attributes = columns[8];
-
-                if (!genomicAccession.equals(refSeqGenomicAccession) && !attributes.contains(refSeqVersionedAccession)) {
-                    continue;
-                }
-
-                String[] attributeSplit = attributes.split(";");
-                String[] targetSplit = attributeSplit[1].split(" ");
-                Integer start = Integer.valueOf(targetSplit[1]);
-                Integer stop = Integer.valueOf(targetSplit[2]);
-
-                for (Region region : regions) {
-                    if (strandType.equals(StrandType.MINUS)) {
-                        if (region.getTranscriptLocation().getStart().equals(stop)
-                                && region.getTranscriptLocation().getStop().equals(start)) {
-                            Location genomicLocation = new Location(Integer.valueOf(genomicStart),
-                                    Integer.valueOf(genomicStop));
-                            genomicLocation.setId(hearsayDAOBean.getLocationDAO().save(genomicLocation));
-                            region.setRegionLocation(genomicLocation);
-                            hearsayDAOBean.getRegionDAO().save(region);
-                        }
-                    } else {
-                        if (region.getTranscriptLocation().getStart().equals(start)
-                                && region.getTranscriptLocation().getStop().equals(stop)) {
-                            Location genomicLocation = new Location(Integer.valueOf(genomicStart),
-                                    Integer.valueOf(genomicStop));
-                            genomicLocation.setId(hearsayDAOBean.getLocationDAO().save(genomicLocation));
-                            region.setRegionLocation(genomicLocation);
-                            hearsayDAOBean.getRegionDAO().save(region);
-                        }
+                String refSeqGenomicAccession = null;
+                for (Identifier identifier : referenceSequences.get(0).getIdentifiers()) {
+                    if (identifier.getSystem().equals("www.ncbi.nlm.nih.gov/genome")) {
+                        refSeqGenomicAccession = identifier.getValue();
+                        break;
                     }
                 }
 
+                logger.debug("refSeqGenomicAccession = {}", refSeqGenomicAccession);
+                for (String line : alignmentsLines) {
+
+                    if (line.startsWith("#")) {
+                        continue;
+                    }
+
+                    // NT_187633.1 RefSeq cDNA_match 278295 278486 192 - . ID=aln45579;Target=NM_000853.3 1 192
+                    // +;gap_count=0;identity=1;idty=1;num_ident=1093;num_mismatch=0;pct_coverage=100;pct_identity_gap=100;pct_identity_ungap=100;score=192
+                    String[] columns = line.split("\t");
+                    String genomicAccession = columns[0];
+                    String genomicStart = columns[3];
+                    String genomicStop = columns[4];
+                    String strand = columns[6];
+                    String attributes = columns[8];
+
+                    if (!genomicAccession.equals(refSeqGenomicAccession)) {
+                        continue;
+                    }
+
+                    if (attributes.contains(refSeqVersionedAccession)) {
+
+                        String[] attributeSplit = attributes.split(";");
+                        String[] targetSplit = attributeSplit[1].split(" ");
+                        Integer start = Integer.valueOf(targetSplit[1]);
+                        Integer stop = Integer.valueOf(targetSplit[2]);
+
+                        for (Region region : regions) {
+                            if (strandType.equals(StrandType.MINUS)) {
+                                if (region.getTranscriptLocation().getStart().equals(stop)
+                                        && region.getTranscriptLocation().getStop().equals(start)) {
+                                    Location genomicLocation = new Location(Integer.valueOf(genomicStart),
+                                            Integer.valueOf(genomicStop));
+                                    genomicLocation.setId(hearsayDAOBean.getLocationDAO().save(genomicLocation));
+                                    region.setRegionLocation(genomicLocation);
+                                    hearsayDAOBean.getRegionDAO().save(region);
+                                }
+                            } else {
+                                if (region.getTranscriptLocation().getStart().equals(start)
+                                        && region.getTranscriptLocation().getStop().equals(stop)) {
+                                    Location genomicLocation = new Location(Integer.valueOf(genomicStart),
+                                            Integer.valueOf(genomicStop));
+                                    genomicLocation.setId(hearsayDAOBean.getLocationDAO().save(genomicLocation));
+                                    region.setRegionLocation(genomicLocation);
+                                    hearsayDAOBean.getRegionDAO().save(region);
+                                }
+                            }
+                        }
+
+                    }
+
+                }
+                hearsayDAOBean.getAlignmentDAO().save(alignment);
             }
-            hearsayDAOBean.getAlignmentDAO().save(alignment);
 
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
