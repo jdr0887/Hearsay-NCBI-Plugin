b9e9ece79e0222996b93c0f3edd20d0bf96ad864
Jason Reilly <jdr0887@gmail.com>
2015-12-15 10:38:03 -0500

    formatting

--- ncbi-commands/src/main/java/org/renci/hearsay/commands/ncbi/PullGenomeReferencesRunnable.java
+++ ncbi-commands/src/main/java/org/renci/hearsay/commands/ncbi/PullGenomeReferencesRunnable.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -20,7 +21,7 @@ import org.slf4j.LoggerFactory;
 
 public class PullGenomeReferencesRunnable implements Runnable {
 
-    private final Logger logger = LoggerFactory.getLogger(PullGenomeReferencesRunnable.class);
+    private static final Logger logger = LoggerFactory.getLogger(PullGenomeReferencesRunnable.class);
 
     private HearsayDAOBeanService hearsayDAOBeanService;
 
@@ -31,76 +32,91 @@ public class PullGenomeReferencesRunnable implements Runnable {
 
     @Override
     public void run() {
-        logger.debug("ENTERING run()");
+        logger.info("ENTERING run()");
+        // File refseqAssemblySummaryFile = FTPUtil.ncbiDownload("/genomes/refseq", "assembly_summary_refseq.txt");
 
-        File refseqAssemblySummaryFile = FTPUtil.ncbiDownload("/genomes/refseq", "assembly_summary_refseq.txt");
+        for (File refseqAssemblySummaryFile : Arrays.asList(
+                FTPUtil.ncbiDownload("/genomes/refseq/vertebrate_mammalian", "assembly_summary.txt"),
+                FTPUtil.ncbiDownload("/genomes/refseq/vertebrate_mammalian", "assembly_summary_historical.txt"))) {
 
-        try (BufferedReader br = new BufferedReader(new FileReader(refseqAssemblySummaryFile))) {
-            // # assembly_accession bioproject biosample wgs_master refseq_category taxid species_taxid
-            // organism_name infraspecific_name isolate version_status assembly_level release_type genome_rep
-            // seq_rel_date asm_name submitter gbrs_paired_asm paired_asm_comp ftp_path
-            String line;
-            while ((line = br.readLine()) != null) {
-                if (line.startsWith("#")) {
-                    continue;
+            try (BufferedReader br = new BufferedReader(new FileReader(refseqAssemblySummaryFile))) {
+                // # assembly_accession bioproject biosample wgs_master refseq_category taxid species_taxid
+                // organism_name infraspecific_name isolate version_status assembly_level release_type genome_rep
+                // seq_rel_date asm_name submitter gbrs_paired_asm paired_asm_comp ftp_path
+                String line;
+                while ((line = br.readLine()) != null) {
+                    if (line.startsWith("#")) {
+                        continue;
+                    }
+                    try (Scanner scanner = new Scanner(line).useDelimiter("\t")) {
+                        String assemblyAccession = scanner.next();
+                        String bioProject = scanner.next();
+                        String bioSample = scanner.next();
+                        String wgsMaster = scanner.next();
+                        String refseqCategory = scanner.next();
+                        String taxId = scanner.next();
+                        String speciesTaxId = scanner.next();
+                        String organismName = scanner.next();
+                        if (!"homo sapiens".equalsIgnoreCase(organismName)) {
+                            continue;
+                        }
+                        String infraspecificName = scanner.next();
+                        String isolate = scanner.next();
+                        String versionStatus = scanner.next();
+                        String assemblyLevel = scanner.next();
+                        String releaseType = scanner.next();
+                        String genomeRep = scanner.next();
+                        String seqReleaseDate = scanner.next();
+                        String asmName = scanner.next();
+                        if (!asmName.startsWith("GR")) {
+                            continue;
+                        }
+                        String submitter = scanner.next();
+                        String gbrsPairedASM = scanner.next();
+                        String pairedASMComp = scanner.next();
+                        String ftpPath = scanner.next();
+
+                        List<GenomeReference> potentiallyFoundGenomeReferenceList = hearsayDAOBeanService.getGenomeReferenceDAO()
+                                .findByName(asmName);
+                        if (CollectionUtils.isNotEmpty(potentiallyFoundGenomeReferenceList)) {
+                            logger.info("GenomeReference is already persisted");
+                            continue;
+                        }
+
+                        Identifier identifier = new Identifier("www.ncbi.nlm.nih.gov/assembly",
+                                getGenomeReferenceAssemblyId(assemblyAccession));
+                        List<Identifier> foundIdentifierList = hearsayDAOBeanService.getIdentifierDAO().findByExample(identifier);
+                        if (CollectionUtils.isNotEmpty(foundIdentifierList)) {
+                            identifier = foundIdentifierList.get(0);
+                        } else {
+                            identifier.setId(hearsayDAOBeanService.getIdentifierDAO().save(identifier));
+                        }
+                        logger.debug(identifier.toString());
+
+                        GenomeReference genomeReference = new GenomeReference(asmName);
+                        genomeReference.setId(hearsayDAOBeanService.getGenomeReferenceDAO().save(genomeReference));
+                        logger.debug(genomeReference.toString());
+
+                        genomeReference.getIdentifiers().add(identifier);
+                        hearsayDAOBeanService.getGenomeReferenceDAO().save(genomeReference);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
-                try (Scanner scanner = new Scanner(line).useDelimiter("\t")) {
-                    String assemblyAccession = scanner.next();
-                    String bioProject = scanner.next();
-                    String bioSample = scanner.next();
-                    String wgsMaster = scanner.next();
-                    String refseqCategory = scanner.next();
-                    String taxId = scanner.next();
-                    String speciesTaxId = scanner.next();
-                    String organismName = scanner.next();
-                    if (!"homo sapiens".equalsIgnoreCase(organismName)) {
-                        continue;
-                    }
-                    String infraspecificName = scanner.next();
-                    String isolate = scanner.next();
-                    String versionStatus = scanner.next();
-                    String assemblyLevel = scanner.next();
-                    String releaseType = scanner.next();
-                    String genomeRep = scanner.next();
-                    String seqReleaseDate = scanner.next();
-                    String asmName = scanner.next();
-                    if (!asmName.startsWith("GR")) {
-                        continue;
-                    }
-                    String submitter = scanner.next();
-                    String gbrsPairedASM = scanner.next();
-                    String pairedASMComp = scanner.next();
-                    String ftpPath = scanner.next();
-
-                    List<GenomeReference> potentiallyFoundGenomeReferenceList = hearsayDAOBeanService.getGenomeReferenceDAO()
-                            .findByName(asmName);
-                    if (CollectionUtils.isNotEmpty(potentiallyFoundGenomeReferenceList)) {
-                        logger.info("GenomeReference is already persisted");
-                        continue;
-                    }
-
-                    Identifier identifier = new Identifier("www.ncbi.nlm.nih.gov/assembly",
-                            getGenomeReferenceAssemblyId(assemblyAccession));
-                    identifier.setId(hearsayDAOBeanService.getIdentifierDAO().save(identifier));
-                    logger.info(identifier.toString());
-
-                    GenomeReference genomeReference = new GenomeReference(asmName);
-                    genomeReference.getIdentifiers().add(identifier);
-                    genomeReference.setId(hearsayDAOBeanService.getGenomeReferenceDAO().save(genomeReference));
-                    logger.info(genomeReference.toString());
-
-                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+            refseqAssemblySummaryFile.delete();
         }
-        refseqAssemblySummaryFile.delete();
+        logger.info("LEAVING run()");
     }
 
     private String getGenomeReferenceAssemblyId(String assemblyAccession) {
         String ret = null;
         try {
             String url = String.format("http://www.ncbi.nlm.nih.gov/assembly/%s?report=xml&format=text", assemblyAccession);
+            logger.info("url: {}", url);
             URL obj = new URL(url);
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
             con.setRequestMethod("GET");
